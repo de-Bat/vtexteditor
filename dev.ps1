d8e6d0555e7572240cc18beedd6abc10a3da021f
Peter Parker <xanderon.git@hotmail.com>
2026-05-09 20:10:27 +0300

    fix(dev): run vision service with uvicorn instead of python main.py directly

--- dev.ps1
+++ dev.ps1
@@ -4,4 +4,4 @@ $root = $PSScriptRoot
 
 wt `
   new-tab --title "Server + Client" --startingDirectory $root pwsh -NoExit -Command "npm run dev" `; `
-  split-pane --title "Vision Service" --startingDirectory "$root\vision-service" pwsh -NoExit -Command "python main.py"
+  split-pane --title "Vision Service" --startingDirectory "$root\vision-service" pwsh -NoExit -Command "python -m uvicorn main:app --port 3001 --host 127.0.0.1 --reload"
